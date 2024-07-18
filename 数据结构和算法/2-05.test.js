3b321d71d2a313a47f80bc4c287935dadddfa67e
wangluyao <wangluyao959277@163.com>
2024-07-18 20:24:05 +0800

    feat:补充单元测试

--- 数据结构和算法/2-05.test.js
+++ 数据结构和算法/2-05.test.js
@@ -10,6 +10,25 @@ describe('将一个数组旋转K步', () => {
         const k = 3;
         expect(rotate1(arr, k)).toEqual([5, 6, 7, 1, 2, 3, 4]);
     })
+
+    it('数组为空', () => {
+        const arr = [];
+        const k = 3;
+        expect(rotate1(arr, k)).toEqual([])
+    })
+
+    it('key 为负数', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7];
+        const k = -3;
+        expect(rotate1(arr, k)).toEqual([5, 6, 7, 1, 2, 3, 4])
+    })
+
+    it('key 超过数组长度', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7];
+        const k = 9;
+        expect(rotate1(arr, k)).toEqual([6, 7, 1, 2, 3, 4, 5])
+    })
+
     it('rotate2 正常情况', () => {
         const arr = [1, 2, 3, 4, 5, 6, 7];
         const k = 3;
