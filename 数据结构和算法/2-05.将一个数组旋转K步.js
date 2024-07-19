b4a46a1e612869c166780722aa5eb53222983f58
wangluyao <wangluyao959277@163.com>
2024-07-19 13:40:54 +0800

    feat:增加备注

--- 数据结构和算法/2-05.将一个数组旋转K步.js
+++ 数据结构和算法/2-05.将一个数组旋转K步.js
@@ -1,20 +1,25 @@
 /**
  * 将一个数组旋转K步
+ * js数组API的时间复杂度大全
+ * https://blog.csdn.net/weixin_49035434/article/details/130193844
  */
 
 // 方法1: 将数组尾部依次pop , 放在头部unshift
 function rotate1(arr, key) {
-    //时间复杂度 O(n)
+    // 时间复杂度 O(n^2)
+    // 空间复杂度O(1)
     if (!key || !arr.length) return arr
     const step = Math.abs(key % arr.length)
     for (let i = 0; i < step; i++) {
-        arr.unshift(arr.pop())
+        arr.unshift(arr.pop()) //  数组是一个有序数组,unshift操作非常慢 unshift时间复杂度为O(n)
     }
     return arr
 }
 
 // 方法2: 分割数组
 function rotate2(arr, key) {
+    // 时间复杂度 O(1)
+    // 空间复杂度 O(n)
     if (!key || !arr.length) return arr;
     const step = Math.abs(key % arr.length);
     const tempArr = arr.slice(-step);
